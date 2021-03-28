3c8c473d80f7a58c6167703ebfeb5c3ecbc6a060
xiaozhucantfly <1010976157@qq.com>
2021-03-28 15:48:07 +0800

    分支测试

--- book-mgr-fe/src/views/Auth/index.js
+++ book-mgr-fe/src/views/Auth/index.js
@@ -1,6 +1,14 @@
 import { defineComponent } from 'vue';
-
+// impotent组件包
+import { UserOutlined, LockOutlined, MailOutlined   } from '@ant-design/icons-vue'
 export default defineComponent({
+    //进行icon注册
+    components: {
+        UserOutlined,
+        LockOutlined,
+        MailOutlined
+    },
+
     setup() {
 
     },
--- book-mgr-fe/src/views/Auth/index.vue
+++ book-mgr-fe/src/views/Auth/index.vue
@@ -1,32 +1,60 @@
 <template>
     <div class="auth">
+        <!-- 背景 -->
+        <div class="bg"></div>
         <!-- 表头 -->
         <div class="title-info">
-            <img src="" alt="">
-            <div class="title">图书管理系统 后台</div>
+            <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.51yuansu.com%2Fpic3%2Fcover%2F03%2F10%2F59%2F5b487a5064630_610.jpg&refer=http%3A%2F%2Fpic.51yuansu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619506300&t=7d8f0ac500f05a412a5ee9156397e46f" alt="">
+            <h2 class="title">图书管理系统 后台</h2>
         </div>
         <!-- 表单部分 -->
         <div class="form">
             <a-tabs >
+                <!-- 登陆 -->
             <a-tab-pane key="1" tab="登陆">
                 <div class="item">
-                    <a-input placeholder="账户"/>
+                    <a-input size="large" placeholder="账户">
+                        <template #prefix><UserOutlined /></template>
+                    </a-input>
                 </div>
                 <div class="item">
-                        <a-input placeholder="密码"/>
+                    <a-input size="large" placeholder="密码">
+                        <template #prefix><LockOutlined /></template>
+                    </a-input>
                 </div>
                 <div class="item">
                         <a href="">忘记密码</a>
                 </div>
                 <div class="item">
-                        <a-button type="primary">
+                        <a-button size="large" type="primary">
                             登陆
                         </a-button>
                 </div> 
                
             </a-tab-pane>
+            <!-- 注册 -->
             <a-tab-pane key="2" tab="注册">
-                assign
+                <div class="item">
+                    <a-input size="large" placeholder="账户">
+                        <template #prefix><UserOutlined /></template>
+                    </a-input>
+                </div>
+                <div class="item">
+                    <a-input size="large" placeholder="密码">
+                        <template #prefix><LockOutlined /></template>
+                    </a-input>
+                </div>
+                <div class="item">
+                    <a-input size="large" placeholder="邀请码">
+                        <template #prefix><MailOutlined /></template>
+                    </a-input>
+                </div>
+              
+                <div class="item">
+                        <a-button size="large" type="primary">
+                            注册
+                        </a-button>
+                </div> 
             </a-tab-pane>
         </a-tabs>
         </div>
